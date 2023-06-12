e4fc435e965f16b178b502de2c01c609c449195b
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-06-12 12:05:33 -0700

    10. Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,21 +1,4 @@
-import ctypes
-
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = [not i % 2 for i in my_list]
+    return (new_list)
