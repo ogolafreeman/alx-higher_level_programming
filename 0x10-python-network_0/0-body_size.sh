cd44ba4b0bd07ab2b525f7ba98e48a2081c374cf
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-04-26 23:26:00 +0300

    Network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# Bash script send a request to an URL with curl,
-# and displays the size of the body of the response
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-response=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
-
-# Display the size of the response body
-echo "Size of the response body: $response bytes"
-
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
