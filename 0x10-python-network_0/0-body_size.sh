00341f6ebcdfa983de0583bc4bd1a1a567c8f137
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-04-26 21:49:00 +0300

    delete

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # Bash script send a request to an URL with curl,
 # and displays the size of the body of the response
-curl -s "$1" | wc -c
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+
+response=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
+
+# Display the size of the response body
+echo "Size of the response body: $response bytes"
+
