af10f4b7801351ed00fff7dc211c1ccf218fd31e
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-04-26 23:14:45 +0300

    networ

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def main():
+    """
+    Funtion to print a response of a specific url
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+if __name__ == "__main__":
+    main()
