f40c019d85b113d7f627e2dc2115479a56a009fe
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-04-26 15:58:49 -0700

    network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,8 +8,7 @@ from sys import argv
 
 def main(argv):
     """
-    Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
+    Sends a POST request to the passed URL with the email as a parameter
     """
     values = {'email': argv[2]}
     url = argv[1]
