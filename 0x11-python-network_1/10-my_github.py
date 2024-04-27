e56564a0ae67897708ba07084e3ad97aaef1bb9c
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-04-27 00:17:11 -0700

    network_1_fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
+"""
+Uses the GitHub API to display a GitHub ID based on given credentials.
 """
 import sys
 import requests
